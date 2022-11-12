113dd80ae003dfa0cb178c108e6b02f19909aed0
Rodolfo Braquetes <braquetesss@gmail.com>
2022-11-12 17:08:06 -0600

    build

---
+++ dist/socket/assets/css/script.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+  $("#rojo").click(function () {
+    //var cssa = $("#a").css({'background-color':'','color':''});
+    // var cssa = $("#b").css("background-color");
+    // alert(cssa);
+    $("#monito").css({
+      background: "url('sombrero.png') 0% 0% / cover",
+      filter: "hue-rotate(0deg) brightness(120%)",
+    });
+  });
+  $("#verde").click(function () {
+    //var cssa = $("#a").css({'background-color':'','color':''});
+    // var cssa = $("#b").css("background-color");
+    // alert(cssa);
+    $("#monito").css({
+      background: "url('sombrero.png') 0% 0% / cover",
+      filter: "hue-rotate(150deg) brightness(100%)",
+    });
+  });
+  $("#amarillo").click(function () {
+    //var cssa = $("#a").css({'background-color':'','color':''});
+    // var cssa = $("#b").css("background-color");
+    // alert(cssa);
+    $("#monito").css({
+      background: "url('sombrero.png') 0% 0% / cover",
+      filter: "hue-rotate(60deg) brightness(500%)",
+    });
+  });
+  $("#azul").click(function () {
+    //var cssa = $("#a").css({'background-color':'','color':''});
+    // var cssa = $("#b").css("background-color");
+    // alert(cssa);
+    $("#monito").css({
+      background: "url('sombrero.png') 0% 0% / cover",
+      filter: "hue-rotate(220deg) brightness(80%)",
+    });
+  });
+  $("#cafe").click(function () {
+    //var cssa = $("#a").css({'background-color':'','color':''});
+    // var cssa = $("#b").css("background-color");
+    // alert(cssa);
+    $("#monito").css({
+      background: "url('sombrero.png') 0% 0% / cover",
+      filter: "hue-rotate(0deg) brightness(50%)",
+    });
+  });
+  $("#blanco").click(function () {
+    //var cssa = $("#a").css({'background-color':'','color':''});
+    // var cssa = $("#b").css("background-color");
+    // alert(cssa);
+    $("#monito").css({
+      background: "url('white.png') 0% 0% / cover",
+      filter: "hue-rotate(0deg) brightness(100%)",
+    });
+  });
+});
